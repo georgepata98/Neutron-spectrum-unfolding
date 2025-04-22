f7a86f3cb91b8d8a5bd6a826642591059850a702
George <122010570+georgepata98@users.noreply.github.com>
2025-04-22 19:04:44 +0300

    Update GRNN_personalizat.py

--- GRNN_personalizat.py
+++ GRNN_personalizat.py
@@ -4,14 +4,16 @@ from sklearn.metrics import mean_squared_error
 from sklearn.neighbors import KNeighborsRegressor
 import matplotlib.pyplot as plt
 
-random_gen = True  # generare de date random
+
+random_gen = False
 if random_gen == True:
+    """ Generarea de date random pentru phi si R """
     def generate_random_data(n_samples, n_energy_groups):
         """ Se genereaza date in [0,1), argumentii din rand() sunt nr. de linii, respectiv coloane
-        n_samples=nr. linii; n_energy_groups=nr. coloane """
+        n_samples = nr. linii; n_energy_groups = nr. coloane """
         spectrum = np.abs(np.random.rand(n_samples, n_energy_groups))  # spectrul simulat
         return spectrum
-    
+
     def response_matrix(n_detectors, n_energy_groups):
         """ Matricea de raspuns cu n_detectors detectori """
         response = np.abs(np.random.rand(n_detectors, n_energy_groups))
@@ -21,23 +23,76 @@ if random_gen == True:
     n_samples = 251       # numarul de spectre
     n_energy_groups = 53  # numarul de bini
     n_detectors = 8       # numarul de detectori
-    
     phi = generate_random_data(n_samples, n_energy_groups)
     R = response_matrix(n_detectors, n_energy_groups)
     C = phi @ R.T  # nr. de counturi (R.T = R transpus)
 
 else:
-    C = phi @ R  # daca pe linii este R(E), fiecare coloana fiind un alt detector
+    """ Spectre de neutroni din IAEA TRS403 """
+    filename_phi = input("Nume fisier .csv cu fluentele de neutroni (fara extensia .csv): ")
+    filename_R = input("Nume fisier .csv cu matricea de raspuns (fara extensia .csv): ")
+    phi_csv, R_csv, n_samples, n_energy_groups, n_detectors = [], [], 0, 0, 0
 
-""" --- Functia predefinita train_test_split() ---
-1) X_train si y_train sunt folosite pentru a antrena modelul GRNN
-2) X_test si y_test sunt pentru a evalua performanta modelului
-3) C = date de input (ratele de numarare calculate mai sus)
-4) phi = date tinta (spectrele de neutroni care vor sa fie prezise)
-5) test_size = 0.25 (25% date de test)
-Observatii: variabilele X_train, X_test iau valori din datele de input C (25% vor fi in X_test); analog y_train si y_test din datele de output phi
-"""
+    ifile = open(filename_phi + ".csv", "r")
+    for i, row in enumerate(ifile):  # i = nr. linie
+        values = row.strip().split(",")
+        float_values = [float(col) for col in values if col.strip()]
+        if i == 0:
+            n_samples = len(float_values)
+        phi_csv.append(float_values)
+        n_energy_groups += 1
+    ifile.close()
+
+    ifile = open(filename_R + ".csv", "r")
+    for i, row in enumerate(ifile):  # i = nr. linie
+        values = row.strip().split(",")
+        float_values = [float(col) for col in values if col.strip()]
+        if i == 0:
+            n_detectors = len(float_values)
+        R_csv.append(float_values)
+    ifile.close()
+
+    phi_csv = np.array(phi_csv)  # transformarea din lista in np.array()
+    R_csv = np.array(R_csv)
+    phi_converted = np.zeros((n_energy_groups-1, n_samples-1))  # umplere initiala cu 0
+    R_converted = np.zeros((n_energy_groups-1, n_detectors-1))  # umplere initiala cu 0
+    """ Convertire phi din unit. de letargie in unit. de energie si scadere dimensiuni """
+    for i in range(n_energy_groups):
+        for j in range(n_samples):
+            if j>0 and i<n_energy_groups-1:  # skip coloanei cu energii si a liniei cu upper bin
+                phi_converted[i][j-1] = phi_csv[i][j] * (np.log(phi_csv[i+1][0]) - np.log(phi_csv[i][0]))
+        for j in range(n_detectors):
+            if j>0 and i<n_energy_groups-1:  # skip coloanei cu energii si a liniei cu upper bin
+                R_converted[i][j-1] = R_csv[i][j]
+
+    n_samples -= 1        # decrementarea lui n_samples cu 1
+    n_energy_groups -= 1  # decrementarea lui n_energy_groups cu 1
+    n_detectors -= 1      # decrementarea lui n_detectors cu 1
+    phi = np.zeros((n_samples, n_energy_groups))
+    R = np.zeros((n_detectors, n_energy_groups))
+    """ Incarcarea lui phi si R """
+    for i in range(n_energy_groups):
+        for j in range(n_samples):
+            phi[j][i] = phi_converted[i][j]
+        for j in range(n_detectors):
+            R[j][i] = R_converted[i][j]
+    """ Calcularea numarului de counturi C """
+    C = phi @ R.T
+    
+    experimental_data = False
+    if experimental_data == True:
+        C_exp = []
+        print(f"Introduceti valorile afisate de cei {n_detectors} detectori:")
+        for i in range(n_detectors):
+            val = float(input(f"C[{i}] = "))  # converteste input in float
+            C_exp.append(val)
+        C_exp = np.array(C_exp)  # transformarea din lista [] in np.array()
+
+
 X_train, X_test, y_train, y_test = train_test_split(C, phi, test_size=0.25, random_state=42)
+# C = date de input (X_train + X_test)
+# phi = date de output (y_train + y_test)
+
 
 """ Clasa Generalized Regression Neural Network (GRNN) """
 class GRNN:
@@ -54,9 +109,6 @@ class GRNN:
         def gaussian(dist):
             return np.exp(- (dist ** 2) / (2 * self.sigma ** 2))
         
-        print("len(X) =", len(X))
-        print("len(X_train) =", len(X_train))
-        
         preds = []
         for x in X:
             """ --- Algoritmul GRNN --- """
@@ -70,20 +122,30 @@ class GRNN:
 """ Antrenare si testare GRNN """
 grnn = GRNN(sigma=0.15)
 grnn.fit(X_train, y_train)
-y_pred = grnn.predict(X_test)  # valorile prezise ale lui y pt. X_test
-print("len(y_pred) =", len(y_pred))  # matrice cu 63 linii
-print("len(y_test) =", len(y_test))  # matrice cu 63 linii
+y_pred = grnn.predict(X_test)  # valorile prezise ale lui X_test
+if experimental_data == True:
+    y_exp = grnn.predict(C_exp)
+
 
 """ Evaluare abatere patratica medie (mse) """
 mse = mean_squared_error(y_test, y_pred)
 print(f"Mean squared error: {mse:.4e}")
 
+
 """ Plotare spectru solutie """
-index = 62  # sunt 63 in total, incepand de la 0
-plt.plot(y_test[index], label='True Spectrum')
-plt.plot(y_pred[index], '--', label='Unfolded Spectrum (GRNN)')
-plt.xlabel("Energy Group")
-plt.ylabel("Fluence [a.u.]")
-plt.title("Neutron Spectrum Unfolding")
-plt.legend()
-plt.show()
+if experimental_data == False:
+    index = int(input("Indexul lui X_test[i] al carui y_test[i] doriti sa il plotati: "))
+    plt.plot(y_test[index], label='True Spectrum', color='red')
+    plt.plot(y_pred[index], '--', label='Unfolded Spectrum (GRNN)', color='blue')
+    plt.xlabel("Energy Group")
+    plt.ylabel("Fluence [a.u.]")
+    plt.title("True vs. Unfolded Spectrum")
+    plt.legend()
+    plt.show()
+else:
+    plt.plot(y_exp[0], label='Unfolded Spectrum (GRNN)')
+    plt.xlabel("Energy Group")
+    plt.ylabel("Fluence [a.u.]")
+    plt.title("Neutron Spectrum")
+    plt.legend()
+    plt.show()
